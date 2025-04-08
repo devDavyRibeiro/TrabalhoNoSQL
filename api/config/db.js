b74044713931a026b138b226d8b62ce29bf7d963
devDavyRibeiro <you@example.comdavy.oliv.ribeiro@gmail.com>
2025-04-08 16:42:42 -0300

    criando o banco de dados e criando a conexao

---
+++ api/config/db.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+let db;
+
+// função que conecta com o Banco
+export async function connectMongoDB(app) {
+    try {
+        const MONGO_URI = app.locals.MONGO_URI //URI do banco do arquivo .env
+        const client = new MongoClient(MONGO_URI); // criação do objeto MongoClient
+        await client.connect();  // conectando com o banco
+        db = client.db(); // cria a instância do banco 
+        app.locals.db = db; //distribuindo o banco para toda aplicação 
+        return db; // retorno da função
+        
+    } catch (error) {
+        console.error('Falha ao conectar ao MongoDB: ', error); // caso haja algum erro, retorna para nós
+        process.exit(1) // saí do processo 
+    }
+}
+export{db}; // exporta o objeto do banco
