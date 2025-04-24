3237ebca4abfde0b4ee7430fac4b6287874263fa
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-04-24 20:51:14 -0300

    Finalizando form Pet

--- api/routes/pet.js
+++ api/routes/pet.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {getPets, getPetID, postPet, deletePets, putPets} from "../controllers/pet.js"; //Importar as funções dentro da controller pet
-import { validateTutor } from "../middlewares/validarTutor.js";
+import { validatePet } from "../middlewares/validarPet.js";
 const router = express.Router()
 
 //Depois de definir o caminho, só chamar o nome da função que você irá usar. EX: getPets irá puxar todo mundo que tiver na collection pet, mas também pode ser deletePet, e assim por diante
@@ -9,9 +9,9 @@ router.get("/", getPets)  //listagem total
 
 router.get("/:id",getPetID) // listagem por ID
 
-router.post("/", validateTutor ,postPet) // criação
+router.post("/", validatePet ,postPet) // criação
 
-router.put("/:id",validateTutor, putPets) //atualização
+router.put("/:id",validatePet, putPets) //atualização
 
 router.delete("/:id",deletePets) // exclusão
 
