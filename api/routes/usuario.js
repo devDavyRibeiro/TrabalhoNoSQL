c3b2f15c487094bf3a5ac092c99a00b789fa8ef9
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-05-22 17:48:17 -0300

    feat: cadastro do usuario com front-end e back-end funcionando

--- api/routes/usuario.js
+++ api/routes/usuario.js
@@ -1,7 +1,6 @@
 import express from "express";
-import { postUsuario } from "../controllers/usuario";
-import { deleteUsuario } from "../controllers/usuario";
-import { validateUsuario } from "../middlewares/validarUsuario";
+import { postUsuario, deleteUsuario } from "../controllers/usuario.js";
+import { validateUsuario } from "../middlewares/validarUsuario.js";
 
 const router = express.Router()
 
@@ -12,3 +11,5 @@ router.post("/", validateUsuario, postUsuario) // criação
 //router.put("/:id", putUsuario) //atualização
 
 router.delete("/:id", validateUsuario, deleteUsuario) // exclusão
+
+export default router
